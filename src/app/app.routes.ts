00604241bba6c3184191af8900ea5f006a92c858
fernandooliveira19 <fer.a.oliveira19@gmail.com>
2021-01-02 21:01:53 -0300

    add login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {Routes} from '@angular/router'
 import { HomeComponent } from './home/home.component'
+import { LoginComponent } from './login/login.component'
 import { TravelerFormComponent } from './travelers/traveler-form/traveler-form.component'
 import { TravelerListComponent } from './travelers/traveler-list/traveler-list.component'
 export const ROUTES : Routes = [
     {path : '' ,  component: HomeComponent},
+    {path : 'login', component:LoginComponent},
     {path : 'traveler-list', component:TravelerListComponent},
     {path : 'traveler-form', component:TravelerFormComponent}
 ]
