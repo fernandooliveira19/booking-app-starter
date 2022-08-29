fc5ed6de9bedbcd0dd3dd4a0b350b2b94c5933d9
Fernando Oliveira <fer.a.oliveira19@gmail.com>
2022-08-29 20:39:44 -0300

    Update booking.service.ts
    disable bookingStatus and paymentStatus fields

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -64,8 +64,8 @@ export class BookingService {
       checkIn: new FormControl(booking.checkIn, [Validators.required]),
       checkOut: new FormControl(booking.checkOut, [Validators.required]),
       amountTotal: new FormControl(booking.amountTotal, [Validators.required]),
-      bookingStatus: new FormControl(booking.bookingStatus, [Validators.required]),
-      paymentStatus: new FormControl(booking.paymentStatus, [Validators.required]),
+      bookingStatus: new FormControl({value: booking.bookingStatus, disabled: true}, [Validators.required]),
+      paymentStatus: new FormControl({value:booking.paymentStatus,disabled:true}, [Validators.required]),
       contractType: new FormControl(booking.contractType, [Validators.required]),
       adults: new FormControl(booking.adults, [Validators.required]),
       children: new FormControl(booking.children, [Validators.required]),
@@ -128,6 +128,7 @@ export class BookingService {
   }
 
   updateBooking(requestBody: Booking): Observable<Booking> {
+    
     let bookingId = requestBody.id;
    
     return this.http.put<Booking>(`${BOOKING_API_GATEWAY}/v1/bookings/${bookingId}`, requestBody)
@@ -140,6 +141,12 @@ export class BookingService {
     return this.http.get<Blob>(url, options)
     .pipe(map(res => new Blob([res], {type :'application/pdf'})));
     
+  }
+
+  finishBooking(requestBody: Booking): Observable<Booking> {
+    let bookingId = requestBody.id;
+   
+    return this.http.put<Booking>(`${BOOKING_API_GATEWAY}/v1/bookings/finish/${bookingId}`, requestBody)
   }
 
 }
